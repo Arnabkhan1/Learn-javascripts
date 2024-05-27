68161df310fb88d63082dce99d8ec362e971ee77
Arnab khan <145889647+Arnabkhan1@users.noreply.github.com>
2024-05-27 07:11:16 +0000

    Learn variable.

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const myName = "Arnab";
+let myEmail = "Khanarnab12345@gmail.com";
+var password = "123456789";
+
+//myName = "anu"; => this is not executable
+
+myEmail = "100xanu.com"
+password = "987654321";
+
+console.log(myEmail);
+console.log(password);
+console.table([myEmail, password])
+
+/*
+prefer not to use var because of issue in scope and function scope
+*/
