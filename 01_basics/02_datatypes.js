81592f45d9d8a65728586fff28aae76e9b5c0cce
Arnab khan <145889647+Arnabkhan1@users.noreply.github.com>
2024-05-27 16:08:18 +0000

    operations & datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3) we are using node.js, not browser
+
+
+// code readability should be high
+
+     // Datatype in javascript
+  
+    // primitive datatype 
+
+let name = "Arnab"; //String
+
+let number = 21; // number
+
+let isfollow = false; // boolean
+
+let age = null; // null
+
+//let school = ; // undefined
+
+// symbol => unique
+
+let num = 123456789; // Bigint
+
+
+ // object 
+
+
+ console.log(typeof null);
+
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+//let score = "33";
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let valueInNumber = Number(score);
+//console.log(typeof score);
+
+let name = "";
+//console.log( typeof name)
+
+let booleanName = Boolean(name);
+console.log(booleanName)
+
+// 1 => true; 2 => false 
+// "" => false
+// "Arnab" => true
+
+let num = 22;
+let numToString = String(num);
+console.log( typeof numToString);
